62f98a3c8c5be4b9a011e33ab49e85778cc34220
SethKalkstein <sethkal7@yahoo.com>
2020-01-15 14:09:42 -0500

    add view, controller, and Model Table logic added for Article

--- cms/src/Controller/ArticlesController.php
+++ cms/src/Controller/ArticlesController.php
@@ -5,9 +5,16 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadComponent("Paginator");
+        $this->loadComponent("Flash");
+        
+    }
     public function index()
     {
-        $this->loadComponent("Paginator");
         $articles = $this->Paginator->paginate($this->Articles->find());
         $this->set(compact('articles'));
     }
@@ -16,5 +23,17 @@ class ArticlesController extends AppController
     {
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
         $this->set(compact('article'));
+    }
+    public function add(){
+        $article = $this->Articles->newEntity();
+        if($this->request->is('post')){
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+            //this is hardcoded now but will be fixed later when authentication is built out
+            if ($this->Articles->save($article)){
+                $this->Flash->success(__("Your article has been saved."));
+                return $this->redirect((['action'=>'index']));
+            }
+        }
+        $this->set('article', $article);
     }
 }
